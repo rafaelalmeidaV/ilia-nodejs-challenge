2345461c41c246ef3d77dd1c45ad60048ec1d0e7
rafaelalmeidav <rafael.4avv@gmail.com>
2026-02-16 10:49:17 -0300

    feature: client docker compose

--- client/src/contexts/AuthProvider.tsx
+++ client/src/contexts/AuthProvider.tsx
@@ -12,12 +12,13 @@ const USER_KEY = 'user_data';
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [user, setUser] = useState<User | null>(() => {
-  const storedUser = storageService.getItem<User>(USER_KEY);
-  const token = tokenService.getToken();
+    const storedUser = storageService.getItem<User>(USER_KEY);
+    const token = tokenService.getToken();
 
-  return storedUser && token ? storedUser : null;
-  });
-  const [isLoading] = useState(true);
+    return storedUser && token ? storedUser : null;
+});
+
+const [isLoading] = useState(false);
 
   const login = (data: AuthResponse) => {
     tokenService.setToken(data.access_token);
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,7 +10,10 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',
     port: 3000,
-    open: true,
+    watch: {
+      usePolling: true,
+    },
   },
 });
